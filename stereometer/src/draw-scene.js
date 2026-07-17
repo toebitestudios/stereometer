3ee3e24f093d96b692dab53a0d5b0f4966eb0e58
LB <toebite.studios@gmail.com>
2026-07-17 09:32:23 +0200

    Add page layout and FOUNDATIONS for camera movement.

---
+++ stereometer/src/draw-scene.js
@@ -0,0 +1,68 @@
+import { mat4 } from "gl-matrix"
+
+/**
+ * @param {WebGLRenderingContext} gl
+ */
+
+function drawScene(gl, programInfo, buffers, rotationX, rotationY){
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearDepth(1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    const fieldOfView = ( 45 * Math.PI ) / 180.0;
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const zNear = 0.1;
+    const zFar = 100.0;
+    const projectionMatrix = mat4.create();
+    mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
+
+    const viewMatrix = mat4.create();
+    mat4.translate(viewMatrix, viewMatrix, [0.0, 0.0, -6.0]);
+    mat4.rotate(viewMatrix, viewMatrix, rotationX, [1, 0, 1]);
+    mat4.rotate(viewMatrix, viewMatrix, rotationY, [0, 1, 1]);
+
+    const modelMatrix = mat4.create();
+        // identitetna matrica
+    //mat4.rotate(modelMatrix, modelMatrix, rotation, [1, 1, 1])
+    
+    setPositionAttribute(gl, buffers, programInfo);
+    setColorAttribute(gl, buffers, programInfo);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
+    gl.useProgram(programInfo.program);
+    gl.uniformMatrix4fv(
+        programInfo.uniformLocations.projectionMatrix,
+        false,
+        projectionMatrix
+    )
+    gl.uniformMatrix4fv(
+        programInfo.uniformLocations.viewMatrix,
+        false,
+        viewMatrix
+    )
+    gl.uniformMatrix4fv(
+        programInfo.uniformLocations.modelMatrix,
+        false,
+        modelMatrix
+    )
+
+    gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
+
+}
+
+function setPositionAttribute(gl, buffers, programInfo){
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+    gl.vertexAttribPointer(
+        programInfo.attribLocations.vertexPosition, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
+}
+
+function setColorAttribute(gl, buffers, programInfo){
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.color);
+    gl.vertexAttribPointer(
+        programInfo.attribLocations.vertexColor, 4, gl.FLOAT, false, 0, 0,);
+    gl.enableVertexAttribArray(programInfo.attribLocations.vertexColor);
+}
+
+export {drawScene};
---
+++ stereometer/src/main.js
@@ -0,0 +1,127 @@
+import { initBuffers } from "./init-buffers"
+import { drawScene } from "./draw-scene"
+
+/**
+ * @param {WebGLRenderingContext} gl
+ */
+
+let rotationX = 0, rotationY = 0;
+let dx = 0, dy = 0;
+
+main();
+
+function main(){
+    const canvas = document.createElement("canvas");
+
+    let rect = document.getElementById("draw-area").getBoundingClientRect();
+    //let w = document.getElementById("draw-area").offsetWidth;
+    canvas.width = rect.width;
+    canvas.height = rect.height;
+    
+    
+    const gl = canvas.getContext("webgl");
+    if (gl == null){
+        alert("Unable to initialize WebGL. Your browser or machine may not support it.", );
+        return;
+    }
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    const vsSource = /*glsl*/`
+        attribute vec4 aVertexPosition;
+        attribute vec4 aVertexColor;
+
+        uniform mat4 uModel;
+        uniform mat4 uView;
+        uniform mat4 uProjection;
+
+        varying lowp vec4 vColor;
+
+        void main(){
+            gl_Position = uProjection * uView * uModel * aVertexPosition;
+            vColor = aVertexColor;
+        }
+    `;
+
+    const fsSource = /*glsl*/`
+        varying lowp vec4 vColor;
+        
+        void main() {
+            gl_FragColor = vColor;
+        }
+    `;
+
+    const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+
+    const programInfo = {
+        program: shaderProgram,
+        attribLocations:{
+            vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
+            vertexColor: gl.getAttribLocation(shaderProgram, "aVertexColor"),
+        },
+        uniformLocations:{
+            projectionMatrix: gl.getUniformLocation(shaderProgram, "uProjection"),
+            viewMatrix: gl.getUniformLocation(shaderProgram, "uView"),
+            modelMatrix: gl.getUniformLocation(shaderProgram, "uModel"),
+        },
+    }
+
+    const buffers = initBuffers(gl);
+
+    let then = 0;
+    function render(movementX, movementY){
+        rotationX += movementX * 0.01
+        rotationY += movementY * 0.01;
+        drawScene(gl, programInfo, buffers, rotationX, rotationY);
+    }
+    canvas.onmousemove = function (e) {
+        requestAnimationFrame(function() {
+            render(e.movementX, e.movementY)
+        }); 
+    }
+    requestAnimationFrame(function() {
+        render(0, 0)
+    });
+    
+    document.getElementById("draw-area").appendChild(canvas);
+
+}
+
+function getDelta(e, oldX, rectLeft, oldY, rectTop){
+    dx = e.clientX - rectLeft - oldX;
+    dy = e.clientY - rectTop - oldY;
+    return {
+
+    }
+}
+
+function initShaderProgram(gl, vSource, fSource){
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vSource);
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fSource);
+
+    const shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)){
+        alert(`Unable to initialize the shader program: ${gl.getProgramInfoLog(shaderProgram,)}`,);
+        return null;
+    }
+
+    return shaderProgram;
+}
+
+function loadShader(gl, type, source){
+    const shader = gl.createShader(type);
+
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert(`An error occurred compiling the shaders: ${gl.getShaderInfoLog(shader)}`,);
+        gl.deleteShader(shader);
+        return null;
+    }
+
+    return shader;
+}
